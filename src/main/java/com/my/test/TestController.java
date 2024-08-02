05d569265805352f8f44ea79ce63fe37fea0afdf
wangx <630350955@qq.com>
2024-08-02 10:12:38 +0800

    init code

---
+++ src/main/java/com/my/test/TestController.java
@@ -0,0 +1,15 @@
+package com.my.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/get")
+    public String test(){
+        return "12222";
+    }
+}
